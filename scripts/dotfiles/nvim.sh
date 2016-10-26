0d1a601bd82f7f9c940e4fa8a903fea822c9e9c5
Christian Titze <christian.titze@student.kit.edu>
2016-10-26 10:45:36 +0200

    Fix problem with tilde and relative paths.

--- scripts/dotfiles/nvim.sh
+++ scripts/dotfiles/nvim.sh
@@ -2,7 +2,7 @@
 
 # Do not run as root!
 
-DIR_DOTFILES="~/.dotfiles"
+DIR_DOTFILES=".dotfiles"
 
 if [ ! -d "${DIR_DOTFILES}" ] ; then
   git clone https://github.com/christian-titze/dotfiles ${DIR_DOTFILES}
@@ -10,14 +10,14 @@ fi
 
 # Link for NeoVim support.
 mkdir -pv ~/.config/nvim/autoload
-ln -risv ${DIR_DOTFILES}/nvim/init.vim ~/.config/nvim/init.vim
-ln -risv ${DIR_DOTFILES}/nvim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
+ln -risv ${DIR_DOTFILES}/nvim/init.vim .config/nvim/init.vim
+ln -risv ${DIR_DOTFILES}/nvim/autoload/plug.vim .config/nvim/autoload/plug.vim
 
 # Link for "legacy" vim support.
 mkdir -pv ~/.vim
-ln -risv ${DIR_DOTFILES}/nvim/init.vim ~/.vim/vimrc
-ln -risv ${DIR_DOTFILES}/nvim/init.vim ~/.vimrc
-ln -risv ~/.config/nvim/autoload ~/.vim/autoload
+ln -risv ${DIR_DOTFILES}/nvim/init.vim .vim/vimrc
+ln -risv ${DIR_DOTFILES}/nvim/init.vim .vimrc
+ln -risv ~/.config/nvim/autoload .vim/autoload
 
 # Automatically install plugins.
 /usr/bin/vim +PlugInstall +qa > /dev/null
--- scripts/dotfiles/zsh.sh
+++ scripts/dotfiles/zsh.sh
@@ -2,15 +2,15 @@
 
 # Do not run as root!
 
-DIR_DOTFILES="~/.dotfiles"
+DIR_DOTFILES=".dotfiles"
 
 if [ ! -d "${DIR_DOTFILES}" ] ; then
   git clone https://github.com/christian-titze/dotfiles ${DIR_DOTFILES}
 fi
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+git clone https://github.com/bhilburn/powerlevel9k.git .oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/chriskempson/base16-shell.git .config/base16-shell
 
-ln -risv ${DIR_DOTFILES}/zsh/zshrc ~/.zshrc
+ln -risv ${DIR_DOTFILES}/zsh/zshrc .zshrc
 
 sudo chsh -s $(which zsh) $(whoami)
