f33099bdacb389c179ddc22e19d8b4e93f7d599a
Christian Titze <christian.titze@student.kit.edu>
2016-12-25 21:31:15 +0100

    Change lib32ncurses5 to lib32ncurses5-dev.

--- scripts/ubuntu/multiarch.sh
+++ scripts/ubuntu/multiarch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install -y libc6-i386 lib32stdc++6 lib32ncurses5 gcc-multilib g++-multilib
+apt-get install -y libc6-i386 lib32stdc++6 lib32ncurses5-dev gcc-multilib g++-multilib
